913f9c20487dac4e5feb8d673886810ac23745ef
veenabalasubramanya <veeba.balasubramanya@gmail.com>
2014-09-29 16:44:47 -0500

    Ascending stack

---
+++ StackCCI/src/com/uta/stacks/SortStack.java
@@ -0,0 +1,16 @@
+package com.uta.stacks;
+
+public class SortStack extends java.util.Stack{
+	
+	public void  sort(Stack input){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack st = new Stack();
+		st.push(69);st.push(77);st.push(30);st.push(75);st.push(15);
+		new SortStack().sort(st);
+	}
+
+}
